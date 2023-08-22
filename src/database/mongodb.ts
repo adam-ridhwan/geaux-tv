92344d268a35b043468771a11256cf8e830135b4
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-08-22 11:28:59 -0400

    fix: mongodb.ts connection

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,44 +1,88 @@
-import { MongoClient } from 'mongodb';
+// import { MongoClient } from 'mongodb';
+//
+// const { MONGODB_URI, MONGODB_DB } = process.env;
+//
+// if (!MONGODB_URI) {
+//   throw new Error('Please define the MONGODB_URI environment variable inside .env');
+// }
+//
+// if (!MONGODB_DB) {
+//   throw new Error('Please define the MONGODB_DB environment variable inside .env');
+// }
+//
+// let cached = global.mongo;
+//
+// if (!cached) {
+//   cached = global.mongo = { conn: null, promise: null };
+// }
+//
+// export async function connectToDatabase() {
+//   if (cached.conn) {
+//     return cached.conn;
+//   }
+//
+//   if (!cached.promise) {
+//     const options = {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     };
+//
+//     cached.promise = MongoClient.connect(MONGODB_URI, options).then(client => {
+//       return {
+//         client,
+//         db: client.db(MONGODB_DB),
+//       };
+//     });
+//   }
+//
+//   // Return the cached promise
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+import { Db, MongoClient } from 'mongodb';
 
 const { MONGODB_URI, MONGODB_DB } = process.env;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env');
-}
-
-if (!MONGODB_DB) {
-  throw new Error('Please define the MONGODB_DB environment variable inside .env');
-}
-
-// @ts-ignore
-let cached = global.mongo;
-
-if (!cached) {
-  // @ts-ignore
-  cached = global.mongo = { conn: null, promise: null };
-}
+let cachedClient: MongoClient;
+let cachedDb: Db;
 
 export async function connectToDatabase() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+  // check the cached.
+  if (cachedClient && cachedDb) {
+    // load from cache
+    return {
+      client: cachedClient,
+      db: cachedDb,
     };
-
-    // @ts-ignore
-    cached.promise = MongoClient.connect(MONGODB_URI, options).then(client => {
-      return {
-        client,
-        db: client.db(MONGODB_DB),
-      };
-    });
   }
 
-  // Return the cached promise
-  cached.conn = await cached.promise;
-  return cached.conn;
+  // set the connection options
+  // const options: MongoClientOptions = {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // };
+
+  // check the MongoDB URI
+  if (!MONGODB_URI) {
+    throw new Error('Define the MONGODB_URI environmental variable');
+  }
+  // check the MongoDB DB
+  if (!MONGODB_DB) {
+    throw new Error('Define the MONGODB_DB environmental variable');
+  }
+
+  // Connect to cluster
+  let client = new MongoClient(MONGODB_URI);
+  await client.connect();
+  let db = client.db(MONGODB_DB);
+
+  // set cache
+  cachedClient = client;
+  cachedDb = db;
+
+  return {
+    client: cachedClient,
+    db: cachedDb,
+  };
 }

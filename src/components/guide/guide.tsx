df383048325fc517af54f17fd4f7d8f4b9b52061
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-08-28 15:44:23 -0400

    styles: adjusted styles

--- src/components/guide/guide.tsx
+++ src/components/guide/guide.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { GRADIENTS } from '@/util/constants/gradients';
-import { incrementGradientObject } from '@/util/incrementGradientObject';
 
 import { Channels } from '@/store/useTvStore';
 import { getAllChannels } from '@/lib/getAllChannels';
@@ -9,6 +8,7 @@ import HorizontalSeparator from '@/components/ui/horizontal-separator';
 import Footer from '@/components/footer/footer';
 import ChannelCategoriesDropdown from '@/components/guide/channel-categories-dropdown';
 import ChannelCategoriesSidebar from '@/components/guide/channel-categories-sidebar';
+import ChannelContainer from '@/components/guide/channel-container';
 import OptionsButton from '@/components/guide/options-button';
 import TvDataConsumer from '@/components/guide/tv-data-consumer';
 
@@ -19,19 +19,25 @@ const Guide = async () => {
 
   const CHANNEL_CATEGORIES = ['Previously watched', ...Object.keys(CHANNELS).map(channel => channel), 'Favorites'];
 
+  let increment = 0;
+  const incrementGradientObject = () => {
+    if (increment === 10) increment = 0;
+    increment++;
+    return increment;
+  };
+
   return (
     <>
       <TvDataConsumer {...{ CHANNELS, CHANNEL_CATEGORIES }} />
 
       <div
-        className='tablet:min-h-[304px] tablet:flex-1 tablet:flex-row tablet:items-start relative flex h-full
-        w-full flex-col items-center overflow-hidden bg-primary-void desktop:h-[304px] desktop:min-h-[304px]'
+        className='relative flex h-full w-full flex-col items-center overflow-hidden bg-primary-void
+        tablet:min-h-[304px] tablet:flex-1 tablet:flex-row tablet:items-start desktop:h-[304px] desktop:min-h-[304px]'
       >
         {/* CHANNEL CATEGORIES */}
         <div
-          className='tablet:flex-col tablet:w-[250px] tablet:min-w-[250px] tablet:justify-start
-          tablet:p-2 tablet:overflow-y-auto tablet:h-full
-          hide-scrollbar flex w-full justify-center bg-primary-void'
+          className='hide-scrollbar flex w-full justify-center bg-primary-void tablet:h-full
+          tablet:w-[250px] tablet:min-w-[250px] tablet:flex-col tablet:justify-start tablet:overflow-y-auto tablet:p-2'
         >
           <ChannelCategoriesDropdown />
           <ChannelCategoriesSidebar />
@@ -40,9 +46,9 @@ const Guide = async () => {
         <div className='hide-scrollbar flex h-full w-full flex-col overflow-y-auto desktop:flex-row'>
           {/* TITLE */}
           {Object.keys(CHANNELS).map(category => (
-            <div key={category} className='tablet:pb-0 flex flex-col pb-[40px] '>
+            <div key={category} className='flex flex-col pb-[40px] tablet:pb-0 '>
               <div className='flex flex-row bg-primary-void pt-[20px]'>
-                <HorizontalSeparator className='relative mb-[20px] bg-accent-light'>
+                <HorizontalSeparator className='relative mb-[32px] bg-accent-light tablet:mb-[20px]'>
                   <span
                     className='absolute left-[16px] top-1/2 -translate-y-1/2 transform whitespace-nowrap rounded-weak
                     border border-accent-light bg-accent-darkest px-3 py-1 text-fs-400 font-bold text-primary-lightest'
@@ -54,22 +60,15 @@ const Guide = async () => {
 
               {/* CHANNELS */}
               <div
-                className='tablet:gap-4 tablet:flex-row tablet:border-none tablet:p-4
-                flex h-full flex-col overflow-auto border-b border-b-primary-dark '
+                className='hide-scrollbar flex h-full flex-col overflow-auto
+                border-b border-b-primary-dark tablet:flex-row tablet:gap-4 tablet:border-none tablet:p-4 '
               >
                 {CHANNELS[category].map(channel => {
                   const index = incrementGradientObject();
                   const firstColor = GRADIENTS[index][0];
                   const secondColor = GRADIENTS[index][1];
                   return (
-                    <div
-                      key={channel.channelNumber}
-                      className='channel-button tablet:rounded-weak tablet:transition-all tablet:hover:scale-[1.075]
-                      tablet:hover:outline tablet:hover:outline-offset-2 relative transform duration-300'
-                      style={{
-                        background: `linear-gradient(120deg, ${firstColor}, ${secondColor})`,
-                      }}
-                    >
+                    <ChannelContainer key={channel.channelNumber} {...{ channel, firstColor, secondColor }}>
                       <ChannelButton channel={channel}>
                         <Image
                           src={channel.channelIcon}
@@ -88,7 +87,7 @@ const Guide = async () => {
                         </div>
                       </ChannelButton>
                       <OptionsButton {...{ channel }} />
-                    </div>
+                    </ChannelContainer>
                   );
                 })}
               </div>
--- src/util/incrementGradientObject.ts
+++
@@ -1,6 +0,0 @@
-let increment = 0;
-export const incrementGradientObject = () => {
-  if (increment === 10) increment = 0;
-  increment++;
-  return increment;
-};
